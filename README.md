e4a9bd4f15893590432e7a5ce3f522d371845e81
Kevin <kevinbalicot@gmail.com>
2023-07-10 20:32:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Session lib
+
+## Installation
+
+```shell
+$ npm install @boutdecode/session
+```
+
+## Yion plugin
+
+For yion : 
+
+```javascript
+const { createApp, createServer } = require('@boutdecode/yion')
+const sessionPlugin = require('@boutdecode/session/yion/session-plugin')
+
+const app = createApp()
+const server = createServer(app, [sessionPlugin])
+
+app.get('/', (req, res) => {
+    req.session // Current session
+})
+
+app.post('/login', (req, res) => {
+    // ... do sign in
+    
+    req.session.user = user
+    req.session.keep(24 * 60 * 60) // Keep session open for 24h
+})
+
+app.get('/admin', (req, res) => {
+    if (req.session.user) {
+        // Connected !
+    }
+    
+    res.redirect('/login')
+})
+
+server.listen(8080)
+```
---
+++ yion/session-plugin.js
@@ -0,0 +1,65 @@
+const { randomUUID } = require('node:crypto')
+const sessions = {}
+
+const startSession = (req, res) => {
+  req.sessionId = randomUUID()
+
+  sessions[req.sessionId] = {
+    id: req.sessionId,
+    flashMessages: {},
+
+    flash (message, level = 'info') {
+      if (!this.flashMessages[level]) {
+        this.flashMessages[level] = []
+      }
+
+      this.flashMessages[level].push(message)
+    },
+
+    getFlashMessages (level) {
+      const messages = this.flashMessages[level] || []
+
+      this.clearFlash()
+
+      return messages
+    },
+
+    clearFlash () {
+      this.flashMessages = {}
+    },
+
+    destroy () {
+      delete sessions[req.sessionId]
+
+      res.set('Set-Cookie', `session=${req.sessionId}; Max-Age=-1`)
+    },
+
+    keep (time = (24 * 60 * 60)) {
+      res.set('Set-Cookie', `session=${req.sessionId}; Max-Age=${time}; Path=/; SameSite=Strict; Secure; HttpOnly`)
+    },
+  }
+
+  res.set('Set-Cookie', `session=${req.sessionId}; Path=/; SameSite=Strict; Secure; HttpOnly`)
+
+  req.session = sessions[req.sessionId]
+}
+
+const parseSessionCookie = (cookies) => {
+  const matches = cookies.match(/session=([a-z0-9-]+)/)
+
+  return matches ? matches[1] : null
+}
+
+module.exports = {
+  type: 'session',
+  handle: (req, res, app, next) => {
+    const cookie = parseSessionCookie(req.headers.cookie || '')
+    if (cookie) {
+      req.session = sessions[cookie]
+    } else {
+      startSession(req, res, {})
+    }
+
+    next()
+  }
+}
